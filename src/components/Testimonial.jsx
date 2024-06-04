71d9a15d732174229f0c9ca09c158ee2d929002c
Julius <ogunniyi.julius12@gmail.com>
2024-06-04 13:51:57 +0100

    update

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -66,7 +66,9 @@ const Testimonial = () => {
 	<div className="container max-w-xl mx-auto">
 		<div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-900 dark:text-gray-100">
 			<img src="https://source.unsplash.com/random/100x100?4" alt="" className="w-20 h-20 rounded-full dark:bg-gray-500" />
-			<blockquote className="max-w-lg text-lg italic font-medium text-center">"Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!"</blockquote>
+			<blockquote className="max-w-lg text-lg italic font-medium text-center">
+      Et, dignissimos obcaecati. Recusandae praesentium doloribus vitae? Rem unde atque mollitia!
+      </blockquote>
 			<div className="text-center dark:text-gray-400">
 				<p>Leroy Jenkins</p>
 				<p>CEO of Company Co.</p>
